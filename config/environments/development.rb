546c288d47eed7d0a008bd8f81920956faec4df4
adrien vidal <andras.adrien@gmail.com>
2018-02-27 13:50:35 +0100

    config heroku path for device configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,7 +35,8 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   # replace by heroku address (heroku.open), and remove port
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'https://airbnb-brain.herokuapp.com'}
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
