a6205e45c761ef891dc366ab0d98fc8412cad1cb
adrien vidal <andras.adrien@gmail.com>
2018-02-27 17:56:33 +0100

    cloudinary fonctionnel

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  mount_uploader :photo, PhotoUploader
+
   has_one :teacher
   has_many :meetings
   devise :database_authenticatable, :registerable,
