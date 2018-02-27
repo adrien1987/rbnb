958ddc36901e8bc821131514974ce84b411973e7
adrien vidal <andras.adrien@gmail.com>
2018-02-27 13:40:40 +0100

    add teachers routes create and new, update teacher controller

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -12,9 +12,13 @@ class TeachersController < ApplicationController
   end
 
   def new
+    @teacher = Teacher.new
   end
 
   def create
+    @teacher = Teacher.new(teacher_params)
+    @teacher.user = current_user
+    redirect_to profiles_path
   end
 
   def edit
@@ -22,4 +26,13 @@ class TeachersController < ApplicationController
 
   def update
   end
+
+  def teacher_params
+    params.require(:teacher).permit(:category, :profession, :description, :hourly_price_cents)
+  end
+
+  def set_teachers
+    @teacher = Teacher.find(params[:category_id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  resources :teachers, only: [:index, :show, :new, :crete, :edit, :update]
+  resources :teachers, only: [:index, :show, :new, :create, :edit, :update]
   get "categories/:category_id/teachers", to: "teachers#index", as: :category_teachers
+
   devise_for :users
   root to: 'categories#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "profiles", to: "profiles#show"
+
 end
