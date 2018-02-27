e172f15d3cb687657fb83812897a2fb726946539
adrien vidal <andras.adrien@gmail.com>
2018-02-27 11:35:48 +0100

    profiles controller + routes + views

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'categories#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+  get "profiles", to: "profiles#show"
 end
